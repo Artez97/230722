e5cb3700000bf424ecab6ebf5fb75ad3234d2298
Artem <ermilintemka@gmail.com>
2022-07-23 00:14:52 +0300

    git add

---
+++ exp1/Program.cs
@@ -0,0 +1,35 @@
+﻿/*
+Задача 64: Задайте значения M и N.
+ Напишите программу, которая выведет все натуральные числа
+  в промежутке от M до N.
+
+M = 1; N = 5. -> ""1, 2, 3, 4, 5""
+
+M = 4; N = 8. -> ""4, 6, 7, 8""
+*/
+
+int M = InputNumbers("Введите M(от): ");
+int N = InputNumbers("Введите N(до): ");
+Console.Write("все натуральные числа в промежутке от M до N:");
+Console.WriteLine();
+int count = M + 1;
+PrintNumber(M, N, count);
+
+
+
+void PrintNumber(int M, int N, int count)
+{
+    while (M < N - 1)
+    {
+        Console.WriteLine(count);
+        M++;
+        count++;
+    }
+}
+
+int InputNumbers(string input)
+{
+    Console.Write(input);
+    int output = Convert.ToInt32(Console.ReadLine());
+    return output;
+}
---
+++ exp2/Program.cs
@@ -0,0 +1,49 @@
+﻿/*Задайте значения M и N.
+ Напишите программу, которая найдёт сумму 
+ натуральных элементов в промежутке от M до N.
+
+M = 1; N = 15-> 120
+M = 4; N = 8. -> 30
+*/
+
+int M = InputNumbers("Введите M(от): ");
+int N = InputNumbers("Введите N(до): ");
+Console.Write("все натуральные числа в промежутке от M до N:");
+Console.WriteLine();
+int count = M + 1;
+int sum = 0;
+PrintNumber(M, N, count);
+
+
+
+void PrintNumber(int M, int N, int count)
+{
+    while (M < N - 1)
+    {
+        Console.WriteLine(count);
+        M++;
+        count++;
+    }
+}
+SumNumber(M, N, count);
+
+
+
+void SumNumber(int M, int N, int count)
+{
+    while (M < N)
+    {
+        sum = sum + M;
+        M++;
+        count++;
+
+    }
+    Console.WriteLine($"Cумма= {sum - 1}");
+}
+
+int InputNumbers(string input)
+{
+    Console.Write(input);
+    int output = Convert.ToInt32(Console.ReadLine());
+    return output;
+}
